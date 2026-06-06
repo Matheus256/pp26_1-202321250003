3f9a44f9812b568618d3a55c1a8829b9affa4948
Matheus Nascimento <matheus.nascimenttoo@gmail.com>
2026-06-06 13:56:42 -0300

    Novas funcionalidades para o gerenciador de politicos

--- Praticas/debate-eleitoral/src/subsystems/mannager/political.cpp
+++ Praticas/debate-eleitoral/src/subsystems/mannager/political.cpp
@@ -1,4 +1,11 @@
+#include <cstdlib>
+#include <format> 
+#include <string>
+
+#include "shared/logger.hpp"
 #include "subsystems/mannager/political.hpp"
+#include "subsystems/debate/debate.hpp"
+
 
 // Adiciona político
 void PoliticalManager::createPolitical(
@@ -17,6 +24,25 @@ PoliticalCollaborator* PoliticalManager::getPolitical(
     }
 
     return nullptr;
+}
+
+void PoliticalManager::drawRequestReplyRight() const{
+    std::string log_mensagem;
+
+    for (auto* political : politicals) {
+        bool op = std::rand() % 3; // 33%
+        if (op == 1) {
+
+            // Cast para usar replyRightEnqueue
+            auto* debateMediator = dynamic_cast<DebateMediator*>(political->getMediator());
+            if (debateMediator != nullptr) {
+                debateMediator->replyRightEnqueue(political);
+            }
+
+            log_mensagem = std::format("O candidato {} do partido {} solicitou Direito de Resposta", political->getName(), political->getParty());
+            Logger::getInstance().info(log_mensagem);
+        }
+    }
 }
 
 // Sorteia político
